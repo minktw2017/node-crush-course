aee74bdbd1e792859976352100394da35c3b5d4f
Mink Wu(win) <minkeiwu@gmail.com>
2021-05-10 11:29:54 +0800

    lesson-6

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for request
+app.listen(5000);
+
+app.get('/', (req, res) =>{
+
+  // res.send('<p>Home Page</p>');
+  res.sendFile('./views/index.html', { root: __dirname});
+
+});
+
+app.get('/about', (req, res) =>{
+
+  // res.send('<p>About Page</p>');
+  res.sendFile('./views/about.html', { root: __dirname});
+});
+
+
+// redirect
+
+app.get('about-us', (req, res) => {
+  res.redirect('/about')
+})
+
+// 404 page
+app.use((req, res) =>{
+  res.status(404).sendFile('./views/404.html', { root: __dirname});
+})
